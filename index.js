39df6d62ae6e07f5f3c40e7b5b6d24f67dbd0388
Thomas Kallevik <thomas@sysco.no>
2014-07-22 09:37:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var po2json = require('po2json');
+var path    = require('path');
+var through = require('through2');
+var gutil   = require('gulp-util');
+var PluginError = gutil.PluginError;
+var File        = gutil.File;
+
+module.exports = function(filename, options) {
+  options = options || {}
+
+  if (!filename) {
+    throw new PluginError('gulp-po2json', 'Missing file name option for gulp-po2json');
+  }
+
+  options.stringify = false;
+
+  var combinedFile;
+  var combinedJson = {};
+
+  function convertPoToJson(file, encoding, callback) {
+    if (!combinedFile) {
+      combinedFile = new File({
+        path: path.join(file.base, filename),
+        base: file.base,
+        cwd: file.cwd
+      });
+    }
+
+    var domainName = path.basename(file.path, path.extname(file.path));
+    var domainData = po2json.parse(file.contents, options);
+
+    combinedJson[domainName] = domainData;
+
+    callback();
+  }
+
+  function flush(callback) {
+    var contents = JSON.stringify(combinedJson, null, options.pretty ? '   ' : null);
+
+    combinedFile.contents = new Buffer(contents)
+
+    this.push(combinedFile);
+
+    callback();
+  }
+
+  return through.obj(convertPoToJson, flush);
+};
